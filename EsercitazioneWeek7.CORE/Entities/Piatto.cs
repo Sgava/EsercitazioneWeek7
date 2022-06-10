cee4a3243ff38f861e6cb4175e642ea0a83ea12e
Gianluca Deias <gianluca.deias@avanade.com>
2022-06-10 09:42:17 +0200

    creazione prime classi

---
+++ EsercitazioneWeek7.CORE/Entities/Piatto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazioneWeek7.CORE.Entities
+{
+    public enum Tipologia 
+    {
+        primo=0,
+        secondo,
+        contorno,
+        Dolce
+    }
+
+    public class Piatto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public Tipologia tipologia { get; set; }
+        public decimal Prezzo { get; set; }
+    }
+}
