c4b9ca6df5c1b7d4c9e353b4f8ca4fbee5de82ce
Gianluca Deias <gianluca.deias@avanade.com>
2022-06-10 10:08:30 +0200

    iniziato EF

---
+++ EsercitazioneWeek7.EF/MasterContext.cs
@@ -0,0 +1,29 @@
+﻿using EsercitazioneWeek7.CORE.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazioneWeek7.EF
+{
+    public class MasterContext : DbContext
+    {
+        public DbSet<Piatto> Piatti { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet<Utente> Utenti { get; set; }
+
+        public MasterContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EsercitazioneWeek7;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
