6ec919dff3b3eca251b8a5428a7ef36cd893506a
Gianluca Deias <gianluca.deias@avanade.com>
2022-06-10 13:50:07 +0200

    aiutoooooo

--- EsercitazioneWeek7.MVC/Program.cs
+++ EsercitazioneWeek7.MVC/Program.cs
@@ -1,15 +1,33 @@
 using EsercitazioneWeek7.CORE.BusinessLayer;
 using EsercitazioneWeek7.CORE.Interfaces;
 using EsercitazioneWeek7.EF;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 builder.Services.AddScoped<IBusinessLayer, MainBusinessLayer>();
-
-
 builder.Services.AddScoped<IRepositoryPiatti, RepositoryPiattiEF>();
+builder.Services.AddScoped<IRepositoryMenu, RepositoryMenuEF>();
+builder.Services.AddScoped<IRepositoryUtenti, RepositoryUtentiEF>();
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = new Microsoft.AspNetCore.Http.PathString("/");
+        option.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Forbidden");
+    }
+    );
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("adm", policy => policy.RequireRole("Admnistrator"));
+    options.AddPolicy("user", policy => policy.RequireRole("User"));
+});
 
 
 var app = builder.Build();
@@ -26,6 +44,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
